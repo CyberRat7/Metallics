5bea7f3bb7ce1c8c5f9c7b13c2eec3154d36128a
w4t3rcs <uimanovmaks@gmail.com>
2023-08-13 11:59:43 +0200

    Redone from .of(Material m) to .copy(Block m)!

--- src/main/java/net/cyber2000/metallics/core/registry/MetallicsBlocks.java
+++ src/main/java/net/cyber2000/metallics/core/registry/MetallicsBlocks.java
@@ -4,8 +4,7 @@ import net.cyber2000.metallics.Metallics;
 import net.cyber2000.metallics.core.util.builder.block.GlassBlockBuilder;
 import net.cyber2000.metallics.core.util.builder.block.MineableBlockBuilder;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
-import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,13 +13,13 @@ import net.minecraftforge.registries.RegistryObject;
 public class MetallicsBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Metallics.MOD_ID);
 
-    public static final RegistryObject<Block> TIN_BLOCK = MineableBlockBuilder.createMineableBlockWithRequiredTool("tin_block", Material.METAL, 1.0f, SoundType.STONE);
-    public static final RegistryObject<Block> RAW_TIN_BLOCK = MineableBlockBuilder.createMineableBlockWithRequiredTool("raw_tin_block", Material.STONE, 1.0f, SoundType.STONE);
-    public static final RegistryObject<Block> TIN_ORE = MineableBlockBuilder.createMineableBlockWithRequiredTool("tin_ore", Material.STONE, 3.0f, SoundType.STONE);
-    public static final RegistryObject<Block> DEEPSLATE_TIN_ORE = MineableBlockBuilder.createMineableBlockWithRequiredTool("deepslate_tin_ore", Material.STONE, 3.0f, SoundType.DEEPSLATE);
+    public static final RegistryObject<Block> TIN_BLOCK = MineableBlockBuilder.createMineableBlockWithRequiredTool("tin_block", Blocks.IRON_BLOCK, 1.0f);
+    public static final RegistryObject<Block> RAW_TIN_BLOCK = MineableBlockBuilder.createMineableBlockWithRequiredTool("raw_tin_block", Blocks.RAW_IRON_BLOCK, 1.0f);
+    public static final RegistryObject<Block> TIN_ORE = MineableBlockBuilder.createMineableBlockWithRequiredTool("tin_ore", Blocks.IRON_ORE, 3.0f);
+    public static final RegistryObject<Block> DEEPSLATE_TIN_ORE = MineableBlockBuilder.createMineableBlockWithRequiredTool("deepslate_tin_ore", Blocks.DEEPSLATE_IRON_ORE, 3.0f);
 
-    public static final RegistryObject<Block> TIN_GLASS = GlassBlockBuilder.createDefaultGlassBlock("tin_glass", 0.6f, SoundType.GLASS);
-    public static final RegistryObject<Block> TIN_GLASS_PANE = GlassBlockBuilder.createDefaultGlassPaneBlock("tin_glass_pane", 0.6f, SoundType.GLASS);
+    public static final RegistryObject<Block> TIN_GLASS = GlassBlockBuilder.createDefaultGlassBlock("tin_glass", 0.6f);
+    public static final RegistryObject<Block> TIN_GLASS_PANE = GlassBlockBuilder.createDefaultGlassPaneBlock("tin_glass_pane", 0.6f);
 
     public static void register(IEventBus eventBus){
         BLOCKS.register(eventBus);
--- src/main/java/net/cyber2000/metallics/core/util/builder/block/MineableBlockBuilder.java
+++ src/main/java/net/cyber2000/metallics/core/util/builder/block/MineableBlockBuilder.java
@@ -3,23 +3,26 @@ package net.cyber2000.metallics.core.util.builder.block;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.RegistryObject;
 
 public class MineableBlockBuilder extends BlockBuilder {
-    public static RegistryObject<Block> createMineableBlock(String name, Material material, float strength, float blastResistance, SoundType sound) {
-        return createBlock(name, () -> new Block(BlockBehaviour.Properties.of(material).strength(strength).explosionResistance(blastResistance).sound(sound)));
+    public static RegistryObject<Block> createMineableBlock(String name, Block material, float strength, float blastResistance, SoundType sound) {
+        return createBlock(name, () -> new Block(BlockBehaviour.Properties.copy(material).strength(strength).explosionResistance(blastResistance).sound(sound)));
     }
 
-    public static RegistryObject<Block> createMineableBlock(String name, Material material, float strength, SoundType sound) {
-        return createBlock(name, () -> new Block(BlockBehaviour.Properties.of(material).strength(strength).sound(sound)));
+    public static RegistryObject<Block> createMineableBlock(String name, Block material, float strength, SoundType sound) {
+        return createBlock(name, () -> new Block(BlockBehaviour.Properties.copy(material).strength(strength).sound(sound)));
     }
 
-    public static RegistryObject<Block> createMineableBlockWithRequiredTool(String name, Material material, float strength, float blastResistance, SoundType sound) {
-        return createBlock(name, () -> new Block(BlockBehaviour.Properties.of(material).strength(strength).explosionResistance(blastResistance).sound(sound).requiresCorrectToolForDrops()));
+    public static RegistryObject<Block> createMineableBlockWithRequiredTool(String name, Block material, float strength, float blastResistance, SoundType sound) {
+        return createBlock(name, () -> new Block(BlockBehaviour.Properties.copy(material).strength(strength).explosionResistance(blastResistance).sound(sound).requiresCorrectToolForDrops()));
     }
 
-    public static RegistryObject<Block> createMineableBlockWithRequiredTool(String name, Material material, float strength, SoundType sound) {
-        return createBlock(name, () -> new Block(BlockBehaviour.Properties.of(material).strength(strength).sound(sound).requiresCorrectToolForDrops()));
+    public static RegistryObject<Block> createMineableBlockWithRequiredTool(String name, Block material, float strength, SoundType sound) {
+        return createBlock(name, () -> new Block(BlockBehaviour.Properties.copy(material).strength(strength).sound(sound).requiresCorrectToolForDrops()));
+    }
+
+    public static RegistryObject<Block> createMineableBlockWithRequiredTool(String name, Block material, float strength) {
+        return createBlock(name, () -> new Block(BlockBehaviour.Properties.copy(material).strength(strength).requiresCorrectToolForDrops()));
     }
 }
